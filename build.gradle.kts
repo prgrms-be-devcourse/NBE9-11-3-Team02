a1452ae1ade389f4689131c8603ad3e6116213a8
hyhdb <kkminwoo@gmail.com>
2026-05-16 19:02:21 +0900

    rest 토큰 entity 변환

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.3.20"           // 추가
     kotlin("plugin.spring") version "2.3.20" // Spring 쓰면 필수
+    kotlin("plugin.jpa") version "2.3.20"
+    kotlin("plugin.lombok") version "2.3.20"
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -51,6 +53,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
     implementation ("org.springframework.boot:spring-boot-starter-cache")
     implementation ("com.github.ben-manes.caffeine:caffeine")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.withType<Test> {
