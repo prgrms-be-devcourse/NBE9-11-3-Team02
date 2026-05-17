ba819a65b1e16a144b891bfd47673b6f323e887c
gmltmdu <dbtmdgml03@naver.com>
2026-05-18 04:27:08 +0900

    refactor: test 코드 수정

--- src/test/java/com/back/together02be/trade/processor/TradeSellProcessorTest.java
+++ src/test/java/com/back/together02be/trade/processor/TradeSellProcessorTest.java
@@ -4,15 +4,16 @@ import com.back.together02be.asset.entity.UserAccount;
 import com.back.together02be.asset.entity.UserStock;
 import com.back.together02be.asset.repository.UserAccountRepository;
 import com.back.together02be.asset.repository.UserStockRepository;
-import com.back.together02be.stock.entity.StockMarket;
-import com.back.together02be.trade.util.MarketTimeValidator;
 import com.back.together02be.stock.dto.RealtimeStockPrice;
 import com.back.together02be.stock.entity.Stock;
+import com.back.together02be.stock.entity.StockMarket;
 import com.back.together02be.stock.repository.StockRepository;
 import com.back.together02be.stock.service.RealTimeStockPriceStore;
 import com.back.together02be.trade.dto.request.TradeSellReq;
 import com.back.together02be.trade.dto.response.TradeSellRes;
 import com.back.together02be.trade.repository.TradeRepository;
+import com.back.together02be.trade.util.MarketTimeValidator;
+import com.back.together02be.users.entity.Users;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,7 +32,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TradeSellProcessorTest {
@@ -69,9 +71,10 @@ class TradeSellProcessorTest {
     @Test
     @DisplayName("t1: 부분 매도 성공")
     void t1() {
+        Users mockUser = new Users("a","12345678","c");
         Stock stock = new Stock("005930", "삼성전자", StockMarket.KOSPI);
-        UserStock userStock = new UserStock(null, stock, 20L, 10000L);
-        UserAccount account = new UserAccount(null, 1000000L, 0L);
+        UserStock userStock = new UserStock(mockUser, stock, 20L, 10000L);
+        UserAccount account = new UserAccount(mockUser, 1000000L, 0L);
         mockCommonDependencies(stock, userStock, account);
 
         String nowTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss"));
@@ -91,9 +94,10 @@ class TradeSellProcessorTest {
     @Test
     @DisplayName("t2: 전량 매도 성공")
     void t2() {
+        Users mockUser = new Users("a","12345678","c");
         Stock stock = new Stock("005930", "삼성전자", StockMarket.KOSPI);
-        UserStock userStock = new UserStock(null, stock, 20L, 10000L);
-        UserAccount account = new UserAccount(null, 1000000L, 0L);
+        UserStock userStock = new UserStock(mockUser, stock, 20L, 10000L);
+        UserAccount account = new UserAccount(mockUser, 1000000L, 0L);
         mockCommonDependencies(stock, userStock, account);
 
         String nowTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss"));
@@ -112,9 +116,10 @@ class TradeSellProcessorTest {
     @Test
     @DisplayName("t3: 실패 - 가격 변동폭 초과")
     void t3() {
+        Users mockUser = new Users("a","12345678","c");
         Stock stock = new Stock("005930", "삼성전자", StockMarket.KOSPI);
-        UserStock userStock = new UserStock(null, stock, 20L, 10000L);
-        UserAccount account = new UserAccount(null, 1000000L, 0L);
+        UserStock userStock = new UserStock(mockUser, stock, 20L, 10000L);
+        UserAccount account = new UserAccount(mockUser, 1000000L, 0L);
         mockCommonDependencies(stock, userStock, account); // 필수!!
 
         String nowTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss"));
@@ -129,9 +134,10 @@ class TradeSellProcessorTest {
     @Test
     @DisplayName("t4: 실패 - 보유 수량 부족")
     void t4() {
+        Users mockUser = new Users("a","12345678","c");
         Stock stock = new Stock("005930", "삼성전자", StockMarket.KOSPI);
-        UserStock userStock = new UserStock(null, stock, 20L, 10000L);
-        UserAccount account = new UserAccount(null, 1000000L, 0L);
+        UserStock userStock = new UserStock(mockUser, stock, 20L, 10000L);
+        UserAccount account = new UserAccount(mockUser, 1000000L, 0L);
         mockCommonDependencies(stock, userStock, account); // 필수!!
 
         // [추가] 가격 정보가 정상적으로 들어오도록 설정
