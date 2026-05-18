85e7e5c3d9d107aa052522ec4c4dafbca89007c7
jinwoo <realize2578@gmail.com>
2026-05-19 01:24:26 +0900

    refactor : 업적 종류 엔티티, 레포지토리 마이그레이션

--- src/main/java/com/back/together02be/achievement/repository/AchievementRepository.kt
+++ src/main/java/com/back/together02be/achievement/repository/AchievementRepository.kt
@@ -1,10 +1,8 @@
-package com.back.together02be.achievement.repository;
+package com.back.together02be.achievement.repository
 
-import com.back.together02be.achievement.entity.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.back.together02be.achievement.entity.Achievement
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.Optional;
-
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-    Optional<Achievement> findByCode(String targetCode);
+interface AchievementRepository : JpaRepository<Achievement, Long> {
+    fun findByCode(targetCode: String): Achievement? // Optional 대신 Nullable 타입 반환
 }
