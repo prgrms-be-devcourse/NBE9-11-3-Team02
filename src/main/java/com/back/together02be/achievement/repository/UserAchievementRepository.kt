59ccecdcbed0c41d9eaffc15ca87c3e33a074cfc
jinwoo <realize2578@gmail.com>
2026-05-19 01:24:32 +0900

    refactor : 유저 달성 업적 엔티티, 레포지토리 마이그레이션

--- src/main/java/com/back/together02be/achievement/repository/UserAchievementRepository.kt
+++ src/main/java/com/back/together02be/achievement/repository/UserAchievementRepository.kt
@@ -1,13 +1,9 @@
-package com.back.together02be.achievement.repository;
+package com.back.together02be.achievement.repository
 
-import com.back.together02be.achievement.entity.UserAchievement;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.back.together02be.achievement.entity.UserAchievement
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.List;
-
-public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long> {
-    boolean existsByUsersIdAndAchievement_Code(Long usersId, String targetCode);
-
-    // 사용자의 모든 업적 달성 기록 조회 추가
-    List<UserAchievement> findByUsersId(Long usersId);
+interface UserAchievementRepository : JpaRepository<UserAchievement, Long> {
+    fun existsByUsersIdAndAchievement_Code(usersId: Long, targetCode: String): Boolean
+    fun findByUsersId(usersId: Long): List<UserAchievement>
 }
