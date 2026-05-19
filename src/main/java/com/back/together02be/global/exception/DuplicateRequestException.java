ef23cd4af75bd606503dbeff6522b9c23a4ad46e
gioJioh <sjgio0515@naver.com>
2026-05-19 11:15:20 +0900

    refactor: kotlin- globalexceptionhandler

--- src/main/java/com/back/together02be/global/exception/DuplicateRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.back.together02be.global.exception;
-
-public class DuplicateRequestException extends RuntimeException {
-
-    public DuplicateRequestException(String message) {
-        super(message);
-    }
-}
